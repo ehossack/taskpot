e125fba2a2c4a0b2fc91960f753e915e7a16d59a
ehossack <rfc2324c418@gmail.com>
2018-10-02 17:18:55 -0700

    Adding in brian's responses

--- bot_components/timerResponses.js
+++ bot_components/timerResponses.js
@@ -1,6 +1,24 @@
 
 'use strict';
 
+const AS_THEY_SAY_IN_HUB = [
+	'Now I regret touching this...',
+    'This is way more complicated then I expected',
+    'Why was it done this way?',
+    'Is there a simple way to...... oh nvm..',
+    'Why do we have this test?',
+    'This is a nightmare',
+    'It shouldn\'t be this hard',
+    'I feel things should be simpler',
+    'Just another day or two',
+    'Oh I forgot to cherry-pick',
+    'It\'s so sad why it\'s not that easy to make it work.',
+    'Can you provide some logs?',
+    'I don\'t know how to name this, just gonna add Service to the end....',
+    'There\'s a code cut off today?',
+    'Why is this a code red?'
+].map(string => `As they say in hub, "${string}"`);
+
 const TIMER_RESPONSES = [
 	'Coffee is ready!',
 	'Pasktot for all!',
@@ -18,7 +36,7 @@ const TIMER_RESPONSES = [
 	':postal_horn: :groovy: :postal_horn:',
 	':ci: Enjoy a tender moment with a pot of task :ci:',
 	'Thanks @andre.bakker'
-];
+].concat(AS_THEY_SAY_IN_HUB);
 
 module.exports = {
     getPhrase: () => TIMER_RESPONSES[Math.floor(Math.random() * TIMER_RESPONSES.length)]
